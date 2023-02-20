3d6d122a13c5b253fb5f1ecaa79dc0079183d707
Pip <pip.harsveld@hva.nl>
2023-02-20 22:53:55 +0100

    Rewrite js with async await

--- spa/scripts/script.js
+++ spa/scripts/script.js
@@ -1,40 +1,44 @@
-let apikey = "RYeqgpSb";
-let q = "";
-let url = "https://www.rijksmuseum.nl/api/nl/collection?key=" + apikey + q;
 const display = document.querySelector('main');
+const apiKey = "RYeqgpSb";
+const url = "https://www.rijksmuseum.nl/api/nl/collection?key=" + apiKey;
 
-fetch(url).then(function (response) {
-    return response.json()
-}).then(function (json) {
-    let data = json.artObjects;
-    console.log(data)
+async function loadArtObjects() {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        const data = json.artObjects;
 
-    data.forEach(function(artObject) {
-        // Create a new figure to hold the information for each art object
-        const newFigure = document.createElement("figure");
+        data.forEach(function (artObject) {
+            // Create a new figure to hold the information for each art object
+            const newFigure = document.createElement("figure");
 
-        //image
-        const image = document.createElement("img");
-        image.src = artObject.webImage.url;
-        newFigure.appendChild(image);
+            // Image
+            const image = document.createElement("img");
+            image.src = artObject.webImage.url;
+            newFigure.appendChild(image);
 
-        // Create a figcaption to hold the title and artist
-        const figCaption = document.createElement("figcaption");
+            // Create a figcaption to hold the title and artist
+            const figCaption = document.createElement("figcaption");
 
-        //title
-        const title = document.createElement("h2");
-        title.textContent = artObject.title;
-        figCaption.appendChild(title);
+            // Title
+            const title = document.createElement("h2");
+            title.textContent = artObject.title;
+            figCaption.appendChild(title);
 
-        //artist
-        const artist = document.createElement("p");
-        artist.textContent = artObject.principalOrFirstMaker;
-        figCaption.appendChild(artist);
+            // Artist
+            const artist = document.createElement("p");
+            artist.textContent = artObject.principalOrFirstMaker;
+            figCaption.appendChild(artist);
 
-        // Append the figcaption to the figure
-        newFigure.appendChild(figCaption);
+            // Append the figcaption to the figure
+            newFigure.appendChild(figCaption);
 
-        // Append the new figure to the display
-        display.appendChild(newFigure);
-    });
-})
+            // Append the new figure to the display
+            display.appendChild(newFigure);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+loadArtObjects();
