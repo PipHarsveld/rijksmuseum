a2b47a293295e9267ffda629724593205c07acbf
Pip <pip.harsveld@hva.nl>
2023-02-21 12:35:41 +0100

    Add js to get more details about the art objects

--- spa/scripts/script.js
+++ spa/scripts/script.js
@@ -2,6 +2,7 @@ const display = document.querySelector('main');
 const apiKey = "RYeqgpSb";
 const url = "https://www.rijksmuseum.nl/api/nl/collection?key=" + apiKey;
 
+
 // Define an asynchronous function to load the art objects
 async function loadArtObjects() {
     try {
@@ -14,31 +15,52 @@ async function loadArtObjects() {
         // Extract the art objects from the JSON response
         const data = json.artObjects;
 
-        // Loop through each art object and create a new <figure> element
-        data.forEach(function (artObject) {
-            const newFigure = document.createElement("figure");
+        console.log(data)
+
+        // Zelfde als de bovenste 3 regels, alleen dan op een andere manier geschreven.
+        // fetch(url)
+        // .then(response => response.json())
+        // .then(data => {
+        //     console.log(data.artObjects)
+        // })
+
+        // Loop through each art object and create a new <article> element
+        data.forEach(async function (artObject) {
+            const url = "https://www.rijksmuseum.nl/api/nl/collection/" + artObject.objectNumber + "?key=" + apiKey;
+
+            const response = await fetch(url)
+            const data = await response.json();
+
+            console.log(data)
+
+            const newFigure = document.createElement("article");
 
             // Image
             const image = document.createElement("img");
             image.src = artObject.webImage.url;
-            // Append the <img> element to the <figure> element
+            // Append the <img> element to the <article> element
             newFigure.appendChild(image);
-
-            // Create a figcaption to hold the title and artist
-            const figCaption = document.createElement("figcaption");
 
             // Title
             const title = document.createElement("h2");
             title.textContent = artObject.title;
-            figCaption.appendChild(title);
+            newFigure.appendChild(title);
 
             // Artist
             const artist = document.createElement("p");
             artist.textContent = artObject.principalOrFirstMaker;
-            figCaption.appendChild(artist);
+            newFigure.appendChild(artist);
 
-            // Append the figcaption to the figure
-            newFigure.appendChild(figCaption);
+            // // Create a section to hold the tags
+            // const tagContainer = document.createElement("section");
+
+            // // Tags
+            // const artist = document.createElement("p");
+            // artist.textContent = artObject.principalOrFirstMaker;
+            // newFigure.appendChild(artist);
+
+            // // Append the tags section to the article
+            // newFigure.appendChild(tagContainer);
 
             // Append the new figure to the display
             display.appendChild(newFigure);
