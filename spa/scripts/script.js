65c95c5f625eafa92f9365ffed992bd7ac2c7b32
Pip <pip.harsveld@hva.nl>
2023-02-21 20:56:03 +0100

    Add loading state, load description and objecttype api

--- spa/scripts/script.js
+++ spa/scripts/script.js
@@ -2,6 +2,7 @@ const display = document.querySelector('main');
 const apiKey = "RYeqgpSb";
 const url = "https://www.rijksmuseum.nl/api/nl/collection?key=" + apiKey;
 
+display.textContent = "Even geduld, de kunstobjecten worden geladen.";
 
 // Define an asynchronous function to load the art objects
 async function loadArtObjects() {
@@ -17,62 +18,48 @@ async function loadArtObjects() {
 
         console.log(data)
 
-        // Zelfde als de bovenste 3 regels, alleen dan op een andere manier geschreven.
-        // fetch(url)
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log(data.artObjects)
-        // })
+
 
         // Loop through each art object and create a new <article> element
         data.forEach(async function (artObject) {
             const url = "https://www.rijksmuseum.nl/api/nl/collection/" + artObject.objectNumber + "?key=" + apiKey;
 
             const response = await fetch(url)
-            const data = await response.json();
+            const json = await response.json();
 
-            console.log(data)
-
+            // Make elements
             const newFigure = document.createElement("article");
-
-            // Image
             const image = document.createElement("img");
-            image.src = artObject.webImage.url;
-            // Append the <img> element to the <article> element
-            newFigure.appendChild(image);
-
-            // Title
             const title = document.createElement("h2");
-            title.textContent = artObject.title;
-            newFigure.appendChild(title);
-
-            // Artist
-            const artist = document.createElement("p");
-            artist.textContent = artObject.principalOrFirstMaker;
-            newFigure.appendChild(artist);
-
-            // Create a section to hold the tags
+            const artist = document.createElement("h3");
             const tagContainer = document.createElement("ul");
             const listItem = document.createElement("li");
-
-            // Object Type
             const objectType = document.createElement("p");
-            objectType.textContent = artObject.objectTypes; // WERKT NIET!!!!!!!!!
-            listItem.appendChild(objectType);
-
-            tagContainer.appendChild(listItem);
-
-            // Append the tags section to the article
-            newFigure.appendChild(tagContainer);
-
-            // Description
             const description = document.createElement("p");
-            description.textContent = artObject.titles;  // WERKT NIET!!!!!!!!!
+
+            // Add content to the elements
+            image.src = artObject.webImage.url;
+            title.textContent = artObject.title;
+            artist.textContent = artObject.principalOrFirstMaker;
+            objectType.textContent = json.artObject.objectTypes; 
+            description.textContent = json.artObject.titles; 
+
+            // Show the elements
+            newFigure.appendChild(image);
+            newFigure.appendChild(title);
+            newFigure.appendChild(artist);
+            listItem.appendChild(objectType);
+            tagContainer.appendChild(listItem);
+            newFigure.appendChild(tagContainer);
             newFigure.appendChild(description);
 
             // Append the new figure to the display
             display.appendChild(newFigure);
         });
+
+        // Remove loading state
+        display.textContent = "";
+
     } catch (error) {
         console.log(error);
     }
@@ -80,3 +67,4 @@ async function loadArtObjects() {
 
 // Call the loadArtObjects() function to display the art objects
 loadArtObjects();
+
