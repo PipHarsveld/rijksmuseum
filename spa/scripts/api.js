0893a890eb2dcb8eb31bfce274a2123920206cdb
Pip <pip.harsveld@hva.nl>
2023-03-07 18:04:14 +0100

    Add file with all API calls

---
+++ spa/scripts/api.js
@@ -0,0 +1,68 @@
+import CONFIG from './config.js';
+
+async function getDetails(objectNumber) {
+    try {
+        console.log("GETTING DETAILS")
+        const url = "https://www.rijksmuseum.nl/api/nl/collection/" + objectNumber + "?key=" + CONFIG.apiKey;
+
+        const response = await fetch(url);
+
+        if (response.status >= 200 && response.status <= 299) {
+            const json = await response.json();
+            const data = json.artObjects;
+            return data;
+        } else {
+            return "error";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function getAllArtworks() {
+    try {
+        console.log("GETTING ALL ARTWORKS")
+        const url = `${CONFIG.baseURL}?key=${CONFIG.apiKey}`;
+
+        const response = await fetch(url);
+
+        if (response.status >= 200 && response.status <= 299) {
+            const json = await response.json();
+            const data = json.artObjects;
+            return data;
+        } else {
+            return "error";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getSpecificArtworks(objectType) {
+    try {
+        console.log("GETTING PAINTINGS")
+        const url = `${CONFIG.baseURL}?key=${CONFIG.apiKey}&s=${objectType}`;
+
+        const response = await fetch(url);
+
+        if (response.status >= 200 && response.status <= 299) {
+            const json = await response.json();
+            const data = json.artObjects;
+            return data;
+        } else {
+            return "error";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+export default {
+    getDetails,
+    getAllArtworks,
+    getSpecificArtworks,
+}
